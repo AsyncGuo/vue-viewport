81c2bbc9f17033e54efe5c1795851a01c5c968c3
guoxiaobo <xiaobo.guo@kongapi.com>
2018-03-08 17:59:12 +0800

    viewport的demo

--- src/App.vue
+++ src/App.vue
@@ -18,17 +18,59 @@ export default {
 
 <style lang="less">
     @import "./common/styles/index.less";
+    
+    html {
+      margin: 0;
+      padding: 0;
+      height: 100vh;
+      box-sizing: border-box;
+    }
+    body {
+      margin: 0;
+      padding: 0;
+      min-height: 100vh;
+      background-color: #f4f4f4;
+    }
+    * {
+      box-sizing: inherit;
+      &:before,
+      &:after {
+        box-sizing: inherit;
+      }
+    }
     #app{
-        width: 100%;
-        min-height: 100%;
+      width: 100%;
+      height: 100%;
         .fade-in-enter-active, .fade-in-leave-active {
             transition: opacity .5s
         }
         .fade-in-enter, .fade-in-leave-to {
             opacity: 0
         }
-        img {
-          content: normal !important;
-        }
+    }
+    img {
+      width: 100%;
+      height: auto;
+      vertical-align: top;
+      content: normal !important;
+    }
+    [aspectratio] {
+      position: relative;
+    }
+    [aspectratio]::before {
+      content: '';
+      display: block;
+      width: 1px;
+      margin-left: -1px;
+      height: 0;
+    }
+    [aspectratio-content] {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      width: 100%;
+      height: 100%;
     }
 </style>
--- src/components/Car/index.vue
+++ src/components/Car/index.vue
@@ -1,6 +1,136 @@
 <template>
-  <div class="demo">
-    <p>999</p>
+  <div class="viewport">
+    <!-- banner -->
+    <div class="banner" w-750-235 aspectratio aspect-ratio="750/235">
+     <div aspectratio-content>
+      <img src="//gw.alicdn.com/imgextra/i1/82/TB2M_c0jmB0XKJjSZFsXXaxfpXa_!!82-0-luban.jpg_q50.jpg" alt="" width="100%" height="100%">
+     </div>
+    </div>
+    <!-- grid -->
+    <div class="block">
+      <div class="container" flexcontainer>
+       <div class="block-item" w-375-224 aspectratio aspect-ratio="375/224">
+        <div aspectratio-content>
+         <img src="//gw.alicdn.com/mt/TB1gRq9SXXXXXcRXFXXXXXXXXXX-375-224.png" alt="" width="100%" height="100%">
+        </div>
+       </div>
+       <div class="block-item" w-375-224 aspectratio aspect-ratio="375/224">
+        <div aspectratio-content="">
+         <img src="//gw.alicdn.com/mt/TB1PMCHSXXXXXcQaXXXXXXXXXXX-375-224.png" alt="" width="100%" height="100%">
+        </div>
+       </div>
+      </div>
+      <div class="container" flexcontainer>
+       <div class="block-item" w-375-224 aspectratio aspect-ratio="375/224">
+        <div aspectratio-content>
+         <img src="//gw.alicdn.com/mt/TB1HsjfSXXXXXcFXpXXXXXXXXXX-375-224.png" alt="" width="100%" height="100%">
+        </div>
+       </div>
+       <div class="block-item" w-375-224 aspectratio aspect-ratio="375/224">
+        <div aspectratio-content>
+         <img src="//gw.alicdn.com/mt/TB1qrvXSXXXXXakXFXXXXXXXXXX-375-224.png" alt="" width="100%" height="100%">
+        </div>
+       </div>
+    </div>
+    </div>
+    <!-- block -->
+    <div class="block">
+
+        <div class="container" flexcontainer>
+          <div class="block-item" w-499-250 aspectratio aspect-ratio="499/250">
+             <div aspectratio-content>
+              <img src="//gw.alicdn.com/mt/TB1Jk6lSXXXXXXSXpXXXXXXXXXX-499-250.png" alt="" width="100%" height="100%">
+             </div>
+          </div>
+          <div class="block-item" w-250-250 aspectratio aspect-ratio="250/250">
+             <div aspectratio-content>
+              <img src="//gw.alicdn.com/mt/TB11E6hSXXXXXXlXpXXXXXXXXXX-250-250.png" alt="" width="100%" height="100%">
+             </div>
+          </div>
+        </div>
+
+        <div class="container" flexcontainer>
+           <div class="block-item" w-375-246 aspectratio aspect-ratio="375/246">
+              <div aspectratio-content>
+               <img src="//gw.alicdn.com/mt/TB1N017SXXXXXcWXFXXXXXXXXXX-375-246.png" alt="" width="100%" height="100%">
+              </div>
+           </div>
+           <div class="block-item" w-375-246 aspectratio aspect-ratio="375/246">
+              <div aspectratio-content>
+               <img src="//gw.alicdn.com/mt/TB1rC9DSXXXXXagapXXXXXXXXXX-375-246.png" alt="" width="100%" height="100%">
+              </div>
+           </div>
+      </div>
+
+      <div class="container" flexcontainer>
+        <div class="block-item" w-375-246 aspectratio aspect-ratio="375/246">
+           <div aspectratio-content>
+            <img src="//gw.alicdn.com/mt/TB1SFqYSXXXXXcDXVXXXXXXXXXX-375-246.png" alt="" width="100%" height="100%">
+           </div>
+        </div>
+        <div class="block-item" w-375-246 aspectratio aspect-ratio="375/246">
+           <div aspectratio-content>
+            <img src="//gw.alicdn.com/mt/TB13DKESXXXXXarapXXXXXXXXXX-375-246.png" alt="" width="100%" height="100%">
+           </div>
+        </div>
+    </div>
+
+    </div>
+    <div class="block">
+      <div class="container" flexcontainer>
+        <div class="block-item" w-750-250 aspectratio aspect-ratio="750/250">
+           <div aspectratio-content>
+             <img src="//gw.alicdn.com/mt/TB17hvaSXXXXXahXFXXXXXXXXXX-750-250.png" alt="" width="100%" height="100%">
+           </div>
+        </div>
+      </div>
+
+      <div class="container" flexcontainer>
+         <div class="block-item" w-375-246 aspectratio aspect-ratio="375/246">
+            <div aspectratio-content>
+             <img src="//gw.alicdn.com/mt/TB1t7CZSXXXXXcYXVXXXXXXXXXX-375-246.png" alt="" width="100%" height="100%">
+            </div>
+         </div>
+         <div class="block-item" w-375-246 aspectratio aspect-ratio="375/246">
+            <div aspectratio-content>
+             <img src="//gw.alicdn.com/mt/TB1LoOOSXXXXXbPaXXXXXXXXXXX-375-246.png" alt="" width="100%" height="100%">
+            </div>
+         </div>
+      </div>
+
+      <div class="container" flexcontainer>
+       <div class="block-item" w-188-246 aspectratio aspect-ratio="188/246">
+        <div aspectratio-content>
+         <img src="//gw.alicdn.com/mt/TB1Ey5WSXXXXXcJXVXXXXXXXXXX-188-246.png" alt="" width="100%" height="100%">
+        </div>
+       </div>
+       <div class="block-item" w-187-246 aspectratio aspect-ratio="187/246">
+        <div aspectratio-content>
+         <img src="//gw.alicdn.com/mt/TB1FuvcSXXXXXaCXFXXXXXXXXXX-187-246.png" alt="" width="100%" height="100%">
+        </div>
+       </div>
+       <div class="block-item" w-188-246 aspectratio aspect-ratio="188/246">
+        <div aspectratio-content>
+         <img src="//gw.alicdn.com/mt/TB1eDK5SXXXXXXgXVXXXXXXXXXX-188-246.png" alt="" width="100%" height="100%">
+        </div>
+       </div>
+       <div class="block-item" w-187-246 aspectratio aspect-ratio="187/246">
+        <div aspectratio-content>
+         <img src="//gw.alicdn.com/mt/TB16RKLSXXXXXb5aXXXXXXXXXXX-187-246.png" alt="" width="100%" height="100%">
+        </div>
+       </div>
+      </div>
+
+    </div>
+
+    <ul class="block" item>
+      <li>001</li>
+      <li>002</li>
+      <li>003</li>
+      <li>004</li>
+      <li>005</li>
+      <li>006</li>
+    </ul>
   </div>
 </template>
 
@@ -18,7 +148,63 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  p{
-    border: .5px solid black; border-bottom-width: 4px; font-size: 14px; line-height: 20px; position: relative;
+  .block{
+    margin-top: 15px;
+  }
+  [flexcontainer] {
+    display: flex;
+    width: 750px;
+  }
+  [w-750-235] {
+    width: 750px;
+    z-index: 5000;
+  }
+  [w-750-235]{
+    aspect-ratio:'750:250';
+  }
+  [w-375-224] {
+    width: 375px;
+  }
+  [w-375-224] {
+    aspect-ratio: '375:224';
+  }
+  [w-499-250] {
+    width: 499px;
+  }
+  [w-499-250] {
+    aspect-ratio: '499:250'
+  }
+  [w-250-250] {
+    width: 250px;
+  }
+  [w-250-250] {
+    aspect-ratio: '250:250'
+  }
+  [w-375-246] {
+    width: 375px;
+  }
+  [w-375-246] {
+    aspect-ratio: '375:246'
+  }
+  [w-750-250] {
+    width: 750px;
+  }
+  [w-750-250] {
+    aspect-ratio: '750:250'
+  }
+  [w-188-246] {
+    width: 188px;
+  }
+  [w-188-246] {
+    aspect-ratio: '188:246'
+  }
+  [w-187-246] {
+    width: 187px;
+  }
+  [w-187-246] {
+    aspect-ratio: '187:246'
+  }
+  [item] {
+    background-color: #fff;
   }
 </style>
